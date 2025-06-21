7cf907504499fc50c4bdad3c5ff79f59660dee50
Fairouz Khalaf <159648169+fairouz-khalaf@users.noreply.github.com>
2025-06-21 14:16:44 +0300

    add category method

--- lib/categories/add_category.dart
+++ lib/categories/add_category.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_course/components/textformfield.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddCategory extends StatefulWidget {
@@ -12,6 +12,22 @@ class _AddCategoryState extends State<AddCategory> {
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   TextEditingController categoryNameController = TextEditingController();
   TextEditingController categoryDescriptionController = TextEditingController();
+  CollectionReference categories = FirebaseFirestore.instance.collection(
+    'categories',
+  );
+
+  Future<void> addUser() {
+    // Call the user's CollectionReference to add a new user
+    return categories
+        .add({
+          'categoryName': categoryNameController.text, // John Doe
+          'categoryDescription':
+              categoryDescriptionController.text, // Stokes and Sons
+        })
+        .then((value) => print("Category Added"))
+        .catchError((error) => print("Failed to add category: $error"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,33 +39,34 @@ class _AddCategoryState extends State<AddCategory> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomTextForm(
-                hinttext: "Category Name",
-                mycontroller: categoryNameController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter a category name";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              CustomTextForm(
-                hinttext: "Category Description",
-                mycontroller: categoryDescriptionController,
+              // CustomTextForm(
+              //   hinttext: "Category Name",
+              //   mycontroller: categoryNameController,
+              //   validator: (value) {
+              //     if (value == null || value.isEmpty) {
+              //       return "Please enter a category name";
+              //     }
+              //     return null;
+              //   },
+              // ),
+              // SizedBox(height: 20),
+              // CustomTextForm(
+              //   hinttext: "Category Description",
+              //   mycontroller: categoryDescriptionController,
 
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter a category description";
-                  }
-                  return null;
-                },
-              ),
+              //   validator: (value) {
+              //     if (value == null || value.isEmpty) {
+              //       return "Please enter a category description";
+              //     }
+              //     return null;
+              //   },
+              // ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
                   if (formKey.currentState!.validate()) {
                     // Handle the submission logic here
+                    addUser();
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(content: Text("Category added successfully!")),
                     );
